bc764080e2c9744ed0e7b3a860dd1bab26b74073
hibah <hibahmohammed.k@gmail.com>
2026-08-30 09:50:18 +0400

    Prepare production Docker deployment

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -6,23 +6,28 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
+
 import os
-
-from channels.routing import ProtocolTypeRouter
-from notifications.middleware import JWTAuthMiddleware
-from django.core.asgi import get_asgi_application
-
-from .routing import application as websocket_application
-
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
     "config.settings",
 )
 
+from django.core.asgi import get_asgi_application
+
+# Initialize Django before importing anything that accesses Django models.
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter
+
+from notifications.middleware import JWTAuthMiddleware
+from .routing import application as websocket_application
+
+
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
 
         "websocket": JWTAuthMiddleware(
             websocket_application
